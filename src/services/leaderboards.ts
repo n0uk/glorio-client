0037bc51e965dc0026b65213c538f55fde5bac6b
Mikhail Vitsen <mikhailvitsen@gmail.com>
2017-06-07 00:58:58 +0300

    allys are colored green on leaderboard

--- src/services/leaderboards.ts
+++ src/services/leaderboards.ts
@@ -34,7 +34,7 @@ export default class LeaderboardService extends Service {
         let score_data: Array<number> = message.content['scoreData'];
         let name_data: Array<string> = message.content['nameData'];
         let id_data: Array<number> = message.content['idData'];
-        // message.content['teamData'] - team ids (-1 for non-teamed);
+        let teamId_data: Array<number> = message.content['teamData'];
         // message.content['teamNameData'] - team names ("" for non-teamed);
 
         if (id_data.length > 0) {
@@ -42,10 +42,10 @@ export default class LeaderboardService extends Service {
         }
 
         for (let i = 0; i < score_data.length; i++) {
-            innerHTML.push(this.format(i + 1, name_data[i], score_data[i], id_data[i] === this.world.id));
+            innerHTML.push(this.format(i + 1, name_data[i], score_data[i], id_data[i] === this.world.id, teamId_data[i] > -1 && teamId_data[i] === this.world.teamId));
         }
         if (this.currentRank > 9) {
-            innerHTML.push(this.format(this.currentRank + 1, this.currentName, this.currentScore, true));
+            innerHTML.push(this.format(this.currentRank + 1, this.currentName, this.currentScore, true, true));
         }
         this.container.innerHTML = `
         <div class="ui-leaderboard-player is-header">
@@ -62,9 +62,9 @@ export default class LeaderboardService extends Service {
         this.container.style.visibility = flag ? 'visible' : 'hidden';
     }
 
-    private format(num: number, nickname: string, score: number, isActive: boolean): string {
+    private format(num: number, nickname: string, score: number, isActive: boolean, isAlly: boolean): string {
         return `
-        <div class="ui-leaderboard-player ${isActive ? "is-active" : ""}">
+        <div class="ui-leaderboard-player ${isActive ? "is-active" : isAlly ? "is-ally" : ""}">
             <span class="player-rank">#${num}</span>
             <span class="player-name">${escapeHtml(nickname)}</span>
             <span class="player-score">${score}</span>
